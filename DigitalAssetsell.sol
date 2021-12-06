5a1ad09ebe836e65f06371405d45c4633eb0e24e
AnuguRakeshReddy <95645270+AnuguRakeshReddy@users.noreply.github.com>
2021-12-06 22:49:52 +0530

    Update DigitalAssetsell.sol

--- DigitalAssetsell.sol
+++ DigitalAssetsell.sol
@@ -1,9 +1,9 @@
 pragma solidity 0.8.6;
 
-import "https://github.com/0xcert/ethereum-erc721/blob/master/src/contracts/tokens/nf-token-metadata.sol";
-import "https://github.com/0xcert/ethereum-erc721/blob/master/src/contracts/tokens/nf-token-enumerable.sol";
 import "https://github.com/0xcert/ethereum-erc721/blob/master/src/contracts/ownership/ownable.sol";
 import "https://github.com/0xcert/ethereum-erc721/blob/master/src/contracts/utils/erc165.sol";
+import "https://github.com/0xcert/ethereum-erc721/blob/master/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/0xcert/ethereum-erc721/blob/master/src/contracts/tokens/nf-token-enumerable.sol";
 
 interface IERC2981Royalties {
     function royaltyArt(
